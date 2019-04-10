83357b2464a7834227d0d919a477aea1f48cda7c
sahils1130 <sahils1130@gmail.com>
2019-04-10 12:15:03 +0530

    Add files via upload

---
+++ visualization.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+
+#Frequency of word category
+p_hist = hist(table_final$Positive, xlab = "Positive tweets", col=rainbow(10), main = 'Histogram of Positive tweets') #Positive
+n_hist = hist(table_final$Negative, xlab = "Neg ative tweets", col=rainbow(10), main = 'Histogram of Negative tweets') #Negative
+
+#Pie : Sentiment Categories
+pVal = c(value_outst, value_vgood, value_good, value_neutral, value_bad, value_vbad, value_awful)
+Sentiment = c("Outstanding", "Great", "Good", "Neutral", "Bad", "Poor", "Awful")
+Percent = c(round(pVal/sum(pVal)*100))
+
+df = data.frame(Sentiment, Percent)
+df$Sentiment = factor(df$Sentiment,
+                      levels = c("Outstanding","Great","Good","Neutral", "Bad", "Poor", "Awful"))
+
+p = ggplot(data = df, aes(Sentiment, Percent, fill=Sentiment)) + 
+  ggtitle("Sentiment Category") +
+  geom_bar(stat = "identity")
+
+pie = p + coord_polar("x", start=0)
+pie
+p
